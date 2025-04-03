2347b014dad051df012e908b812e32ce116f9d3b
Thalys M <thalyscorreia@hotmail.com>
2025-04-03 18:30:14 -0300

    feat: modal para buscar filmes

---
+++ src/components/ModalSearch.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import Modal from "./Modal.vue";
+import Input from "./Input.vue";
+import CardResult from "./CardResult.vue";
+
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+const emit = defineEmits<{
+  "update:modelValue": [value: boolean];
+}>();
+const internalValue = ref(false);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    internalValue.value = value;
+  },
+  {
+    immediate: true,
+  }
+);
+watch(
+  () => internalValue.value,
+  (value) => {
+    emit("update:modelValue", value);
+  }
+);
+</script>
+<template>
+  <Modal v-model="internalValue" @onClose="internalValue = false">
+    <Input> </Input>
+    <div class="cards-results-search">
+      <div class="card-result">
+        <CardResult
+          hiddenButton
+          @click=""
+          image="https://image.tmdb.org/t/p/w600_and_h900_bestv2/zBiHKhXklvTFwj4M1uEUcQGAVJ.jpg"
+        />
+        <p class="movie-title">Avatar: O último mestre do ar</p>
+      </div>
+      <div class="card-result">
+        <CardResult
+          hiddenButton
+          @click=""
+          image="https://image.tmdb.org/t/p/w600_and_h900_bestv2/zBiHKhXklvTFwj4M1uEUcQGAVJ.jpg"
+        />
+        <p class="movie-title">Avatar: O último mestre do ar</p>
+      </div>
+      <div class="card-result">
+        <CardResult
+          hiddenButton
+          @click=""
+          image="https://image.tmdb.org/t/p/w600_and_h900_bestv2/zBiHKhXklvTFwj4M1uEUcQGAVJ.jpg"
+        />
+        <p class="movie-title">Avatar: O último mestre do ar</p>
+      </div>
+      <div class="card-result">
+        <CardResult
+          hiddenButton
+          @click=""
+          image="https://image.tmdb.org/t/p/w600_and_h900_bestv2/zBiHKhXklvTFwj4M1uEUcQGAVJ.jpg"
+        />
+        <p class="movie-title">Avatar: O último mestre do ar</p>
+      </div>
+    </div>
+  </Modal>
+</template>
+
+<style>
+.cards-results-search {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+  margin-top: 50px;
+}
+.card-result {
+  max-width: min-content;
+}
+.movie-title {
+  font-size: 16px;
+  font-weight: 400;
+  color: #ffffff;
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
