909df33cdc1782e1c605db16fb426e62f77779e1
Thalys M <thalyscorreia@hotmail.com>
2025-04-03 19:39:50 -0300

    feat: configuracao das envs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
-  },
+export default defineConfig((
+  {mode}
+)=>{
+  const env = loadEnv(mode, process.cwd(),'')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") },
+    },
+    define:{
+      'process.env': {
+        'API_URL': env.API_URL,}
+    }
+  }
 })
