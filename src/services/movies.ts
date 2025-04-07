4d786c33820036f1b795d23f248b05f4ed6c1289
Thalys M <thalyscorreia@hotmail.com>
2025-04-07 19:36:09 -0300

    feat: gerando recomendacao

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -5,3 +5,10 @@ import { http } from "./http"
 export const getMovieByName = async (name: string):Promise<AxiosResponse<IMovie[]>> => {
     return await http.get(`/movies/name/${name}`)
 }
+
+export const generateMovies = async (data: {
+    "user1_movies": string[],
+    "user2_movies": string[]
+  }):Promise<AxiosResponse<{recommendations:IMovie[]}>> => {
+    return await http.post(`/recommendations/recommend/`, data)
+}
